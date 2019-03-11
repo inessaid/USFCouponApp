c8b3d8770f76ef6c26901806c7584fc8ba77f033
ines said <inessaid@mail.usf.edu>
2019-03-10 21:57:00 -0400

    Added log out button

--- app/src/main/java/edu/usf/cse/labrador/usfcouponapp/settingsFragment.java
+++ app/src/main/java/edu/usf/cse/labrador/usfcouponapp/settingsFragment.java
@@ -1,5 +1,6 @@
 package edu.usf.cse.labrador.usfcouponapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,17 +8,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import edu.usf.cse.labrador.usfcouponapp.R;
 
 public class settingsFragment extends Fragment {
+    private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+    Button LogOut;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings,null);
+       // return inflater.inflate(R.layout.fragment_settings,null);
+
+        final View rootView = inflater.inflate(R.layout.fragment_settings, container,
+                false);
+
+        LogOut = (Button) rootView.findViewById(R.id.signout);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()==null)
+                {
+                    Intent intent = new Intent(getActivity(),signIn.class);
+                    startActivity(intent);
+                   // startActivity(new Intent(settingsFragment.this, signIn.class));
+                }
+            }
+        };
+        LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // code what to do with button goes here
+                mAuth.signOut();
+            }
+        }
 
 
+        );
+        return rootView;
 
     }
 
